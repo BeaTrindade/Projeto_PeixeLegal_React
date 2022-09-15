a3b9173398a0147585b32e4981ee7b7687d5c16d
João Lucas Soares Almeida <joaolucasssa21@gmail.com>
2022-09-15 10:20:03 -0300

    @Joaolucas404 [Adicionando tela de Contato]{15/09/2022}

--- src/paginas/contato/Contato.tsx
+++ src/paginas/contato/Contato.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import './Contato.css';
-import Navbar from './components/estaticos/navbar/Navbar';
+import Navbar from "../navbar/Navbar";
 
 function Contato() {
     return (
