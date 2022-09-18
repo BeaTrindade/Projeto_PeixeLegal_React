4e643a05512e1a645a7745edad85232286748922
Mauriciocucilhojr <mauriciocucilhojunior@gmail.com>
2022-09-17 21:35:22 -0300

    @MauriciocucilhoJr [Listagem , Catalogo e styles]

--- src/paginas/catalogo/Catalogo.tsx
+++ src/paginas/catalogo/Catalogo.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import Footer from '../../components/estaticos/footer/Footer';
 import Navbar from '../../components/estaticos/navbar/Navbar';
-import ListaComponentes from '../../components/listaComponentes/ListacComponentes';
+import ListaComponentes from '../../components/listaComponentes/ListaComponentes';
+
 import './Catalogo.css'
+
 
 function Produtos() {
 
-    const [categoriaState, setCategoriaState] = useState('PRODUTOS');
+    const [categoriaState, setCategoriaState] = useState('TODOS2');
 
     const onRadioChangeCategory = (e: React.ChangeEvent<HTMLInputElement>) => {
         setCategoriaState(e.currentTarget.value)
+        console.log(categoriaState)
     };
 
     return (
@@ -18,29 +21,29 @@ function Produtos() {
             <div id='page_catalogo'>
 
                 <section>
-
+                
                         <h2>Categoria</h2>
-                        <label htmlFor="Todos Produtos" className='items'>
-                            <input type="radio" name="categoria" id='Todos Produtos' value="" onChange={(e) => onRadioChangeCategory(e)} defaultChecked />
+                        <label>
+                            <input type="radio" name="teste" value="TODOS2" onChange={(e) => onRadioChangeCategory(e)}  defaultChecked />
                             Todos Produtos
                         </label>
 
-                        <label htmlFor="Agua Doce" className='items'>
-                            <input type="radio" name="categoria" id='Agua Doce' value="AGUA DOCE" onChange={(e) => onRadioChangeCategory(e)} />
+                        <label>
+                            <input type="radio" name="teste"  onChange={(e) => onRadioChangeCategory(e)}  />
                             Àgua Doce
                         </label>
 
-                        <label htmlFor="Agua Salgada" className='items'>
-                            <input type="radio" name="categoria" id='Agua Salgada' value="AGUA SALGADA" onChange={(e) => onRadioChangeCategory(e)} />
+                        <label>
+                            <input type="radio" name="teste" onChange={(e) => onRadioChangeCategory(e)}  />
                             Àgua Salgada
                         </label>
 
-                        <label htmlFor="Camarões" className='items'>
-                            <input type="radio" name="categoria" id='Camarões' value="CAMAROES" onChange={(e) => onRadioChangeCategory(e)} />
+                        <label>
+                            <input type="radio" name="teste" onChange={(e) => onRadioChangeCategory(e)}  />
                             Camarões
                         </label>
-                        <label htmlFor="Caranguejos" className='items'>
-                            <input type="radio" name="categoria" id='Caranguejos' value="CARANGUEIJOS" onChange={(e) => onRadioChangeCategory(e)} />
+                        <label>
+                            <input type="radio" name="teste"   />
                             Caranguejos
                         </label>
 
@@ -48,7 +51,6 @@ function Produtos() {
                 </section>
 
                 <section>
-                    <h1>Listagem</h1>
                     <ListaComponentes categoria={categoriaState} />
                 </section>
 
