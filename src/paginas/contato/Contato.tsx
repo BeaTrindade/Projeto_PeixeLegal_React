6d531b7786f8cd9c63e1bab8db557df55d89cd04
Beatriz Trindade <beatriztrindade@hotmail.com>
2022-09-19 08:26:29 -0300

    @BeaTrindade [LinkedIn e Github] {19/09/22 08:26}

--- src/paginas/contato/Contato.tsx
+++ src/paginas/contato/Contato.tsx
@@ -17,74 +17,74 @@ function Contato() {
             <article>
                     <img src="https://media.discordapp.net/attachments/1001812118422695987/1019296129491415090/profile-pic.png" alt="Ana Lu" />
                     <div>
-                        <a href="https://www.linkedin.com/in/matheus-carnizella-nepomuceno/" target='bash'>
+                        <a href="https://www.linkedin.com/in/ana-civita/" target='bash'>
                             <LinkedInIcon className='icone' />
                         </a>
-                        <a href="https://github.com/MatheusCarnizella" target='bash2'>
+                        <a href="https://github.com/Lu-Civita" target='bash2'>
                             <GitHubIcon className='icone' />
                         </a>
                     </div>
-                    <h3>Matheus Carnizella</h3>
+                    <h3>Ana louise Civita</h3>
                     <h4>Developer FullStack;</h4>
-                    <h4>23 anos;</h4>
-                    <h4>Preferência de Back-End.</h4>
+                    <h4>30 anos;</h4>
+                    <h4>Preferência por FRONT-END.</h4>
                 </article>
                 <article>
                     <img src="https://media.discordapp.net/attachments/1001812118422695987/1018623347078213753/bea.png" alt="Beatriz" />
                     <div>
-                        <a href="https://www.linkedin.com/in/matheus-carnizella-nepomuceno/" target='bash'>
+                        <a href="https://www.linkedin.com/in/beatrizgt/" target='bash'>
                             <LinkedInIcon className='icone' />
                         </a>
-                        <a href="https://github.com/MatheusCarnizella" target='bash2'>
+                        <a href="https://github.com/BeaTrindade" target='bash2'>
                             <GitHubIcon className='icone' />
                         </a>
                     </div>
-                    <h3>Matheus Carnizella</h3>
+                    <h3>Beatriz Gomes Trindade</h3>
                     <h4>Developer FullStack;</h4>
-                    <h4>23 anos;</h4>
-                    <h4>Preferência de Back-End.</h4>
+                    <h4>29 anos;</h4>
+                    <h4>Preferência por FRONT-END.</h4>
                 </article>
                 <article>
                     <img src="https://media.discordapp.net/attachments/1001812118422695987/1017878914854559874/unknown.png" alt="Bruno" />
                     <div>
-                        <a href="https://www.linkedin.com/in/matheus-carnizella-nepomuceno/" target='bash'>
+                        <a href="https://www.linkedin.com/in/bpeixoto96/" target='bash'>
                             <LinkedInIcon className='icone' />
                         </a>
-                        <a href="https://github.com/MatheusCarnizella" target='bash2'>
+                        <a href="https://github.com/bpeixoto96" target='bash2'>
                             <GitHubIcon className='icone' />
                         </a>
                     </div>
-                    <h3>Matheus Carnizella</h3>
+                    <h3>Bruno Peixoto</h3>
                     <h4>Developer FullStack;</h4>
-                    <h4>23 anos;</h4>
-                    <h4>Preferência de Back-End.</h4>
+                    <h4>27 anos;</h4>
+                    <h4>Preferência por FRONT-END.</h4>
                 </article>
                 <article>
                     <img src="https://media.discordapp.net/attachments/1001812118422695987/1017895297051017247/profile-pic.png" alt="Henrique" />
                     <div>
-                        <a href="https://www.linkedin.com/in/matheus-carnizella-nepomuceno/" target='bash'>
+                        <a href="https://www.linkedin.com/in/Henrique-de-Andrade-Rodrigues/" target='bash'>
                             <LinkedInIcon className='icone' />
                         </a>
-                        <a href="https://github.com/MatheusCarnizella" target='bash2'>
+                        <a href="https://github.com/EukirneRas" target='bash2'>
                             <GitHubIcon className='icone' />
                         </a>
                     </div>
-                    <h3>Matheus Carnizella</h3>
+                    <h3>Henrique de Andrade Rodrigues</h3>
                     <h4>Developer FullStack;</h4>
-                    <h4>23 anos;</h4>
+                    <h4>27 anos;</h4>
                     <h4>Preferência de Back-End.</h4>
                 </article>
                 <article>
                     <img src="https://media.discordapp.net/attachments/1001812118422695987/1019306319234220113/profile-pic.png" alt="João" />
                     <div>
-                        <a href="https://www.linkedin.com/in/matheus-carnizella-nepomuceno/" target='bash'>
+                        <a href="https://www.linkedin.com/in/joaoolucas/" target='bash'>
                             <LinkedInIcon className='icone' />
                         </a>
-                        <a href="https://github.com/MatheusCarnizella" target='bash2'>
+                        <a href="https://github.com/Joaolucas404" target='bash2'>
                             <GitHubIcon className='icone' />
                         </a>
                     </div>
-                    <h3>Matheus Carnizella</h3>
+                    <h3>João Lucas Soares Almeida</h3>
                     <h4>Developer FullStack;</h4>
                     <h4>23 anos;</h4>
                     <h4>Preferência de Back-End.</h4>
@@ -92,14 +92,14 @@ function Contato() {
                 <article>
                     <img src="https://media.discordapp.net/attachments/1001812118422695987/1017887987503218778/profile-pic_1.png" alt="Juliana" />
                     <div>
-                        <a href="https://www.linkedin.com/in/matheus-carnizella-nepomuceno/" target='bash'>
+                        <a href="https://www.linkedin.com/in/juliana-gondim-/" target='bash'>
                             <LinkedInIcon className='icone' />
                         </a>
-                        <a href="https://github.com/MatheusCarnizella" target='bash2'>
+                        <a href="https://github.com/JuGondim" target='bash2'>
                             <GitHubIcon className='icone' />
                         </a>
                     </div>
-                    <h3>Matheus Carnizella</h3>
+                    <h3>Juliana Gondim </h3>
                     <h4>Developer FullStack;</h4>
                     <h4>23 anos;</h4>
                     <h4>Preferência de Back-End.</h4>
@@ -122,16 +122,16 @@ function Contato() {
                 <article>
                     <img src="https://media.discordapp.net/attachments/1001812118422695987/1018949670816464996/profile-pic.png" alt="Maurucio" />
                     <div>
-                        <a href="https://www.linkedin.com/in/matheus-carnizella-nepomuceno/" target='bash'>
+                        <a href="https://www.linkedin.com/in/mauricio-cucilho-junior/" target='bash'>
                             <LinkedInIcon className='icone' />
                         </a>
-                        <a href="https://github.com/MatheusCarnizella" target='bash2'>
+                        <a href="https://github.com/MauriciocucilhoJr" target='bash2'>
                             <GitHubIcon className='icone' />
                         </a>
                     </div>
-                    <h3>Matheus Carnizella</h3>
+                    <h3>Mauricio Cucilho Junior</h3>
                     <h4>Developer FullStack;</h4>
-                    <h4>23 anos;</h4>
+                    <h4>30 anos;</h4>
                     <h4>Preferência de Back-End.</h4>
                 </article>             
             </main>
